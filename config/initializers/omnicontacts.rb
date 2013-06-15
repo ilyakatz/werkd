247b7c6aee82e9ea8d9dc56dedf3624afcbe6263
Ilya Katz <ilyakatz@gmail.com>
2013-06-15 16:24:32 -0400

    move settings into config file

--- config/initializers/omnicontacts.rb
+++ config/initializers/omnicontacts.rb
@@ -2,6 +2,6 @@ require "omnicontacts"
 
 Rails.application.middleware.use OmniContacts::Builder do
   importer :gmail,
-    "167144929226-43tdrbru81nhctrqjrps2leq5nu6s3ht.apps.googleusercontent.com",
-    "-7BF5WIdnB8aLadPDgZcwK9O"
+    Figleaf::Settings.omnicontacts.google.client,
+    Figleaf::Settings.omnicontacts.google.secret
 end
