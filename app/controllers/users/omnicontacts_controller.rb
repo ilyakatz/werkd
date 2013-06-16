e64cb3481ab19ca849feebf0da5731cd8c568c6f
Ilya Katz <ilyakatz@gmail.com>
2013-06-15 20:22:56 -0400

    show a nicer view for imported emails

--- app/controllers/users/omnicontacts_controller.rb
+++ app/controllers/users/omnicontacts_controller.rb
@@ -1,17 +1,22 @@
 module Users
   class OmnicontactsController < ApplicationController
 
+    before_filter :authenticate_user!
+
     def callback
       Rails.cache.write(cachekey, request.env['omnicontacts.contacts'], expire: 10.minutes)
-      redirect_to method: :show, id: params[:importer]
+      redirect_to action: :show, id: params[:importer]
     end
 
     def index
     end
 
     def show
-      @importer = params[:importer]
+      @importer = params[:id]
       @contacts = Rails.cache.read(cachekey)
+      unless @contacts.present?
+        redirect_to action: :index
+      end
     end
 
     private
--- config/initializers/omnicontacts.rb
+++ config/initializers/omnicontacts.rb
@@ -3,5 +3,6 @@ require "omnicontacts"
 Rails.application.middleware.use OmniContacts::Builder do
   importer :gmail,
     Figleaf::Settings.omnicontacts.google.client,
-    Figleaf::Settings.omnicontacts.google.secret
+    Figleaf::Settings.omnicontacts.google.secret,
+    max_results: 1000
 end
