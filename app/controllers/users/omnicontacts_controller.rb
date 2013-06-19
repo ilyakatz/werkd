8fc2d72946c2e1e73cc58434aa6ed590b2366845
Ilya Katz <ilyakatz@gmail.com>
2013-06-18 23:15:54 -0400

    make the controller lighter

--- app/controllers/users/omnicontacts_controller.rb
+++ app/controllers/users/omnicontacts_controller.rb
@@ -15,14 +15,12 @@ module Users
     def show
       Rails.logger.info("Reading contacts from #{cachekey}")
       @importer = params[:id]
-      contacts = Rails.cache.read(cachekey)
-      cs = ::ContactsService.new(contacts, current_user)
-      cs.save
-      @contacts = cs.contacts
-      Rails.logger.info("Saved #{@contacts.count} into database")
+      @contacts = load_and_persist
+
       unless @contacts.present?
         redirect_to action: :index
       end
+
     end
 
     def failure
@@ -30,6 +28,14 @@ module Users
     end
 
     private
+
+    def load_and_persist
+      contacts = Rails.cache.read(cachekey)
+      cs = ::ContactsService.new(contacts, current_user)
+      cs.save
+      Rails.logger.info("Saved #{contacts.count} into database")
+      cs.contacts
+    end
 
     def cachekey
       "#{WeRKD::Application.config.secret_token}/#{session["session_id"]}/contacts"
