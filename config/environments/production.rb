ff13faafd4226eaa78f6140fe4940fa88967639e
Ilya Katz <ilyakatz@gmail.com>
2013-06-27 19:27:11 -0400

    add host for emails in prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,7 +50,7 @@ WeRKD::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { :host => 'werked.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'werkd.herokuapp.com' }
 
   # Enable threaded mode
   # config.threadsafe!
