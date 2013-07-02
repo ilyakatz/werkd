18710dd2b010938c65fb7138f8cb925c4c246335
Ilya Katz <ilyakatz@gmail.com>
2013-07-02 09:01:08 -0400

    add tests for inviting users to connect

--- features/import_contacts.feature
+++ features/import_contacts.feature
@@ -1,7 +1,19 @@
 Feature: invite contacts
 
   Scenario: I want to invite new users
-    Given PENDING
+    When I login as "ilya@werkd.net"
+    And google returns my contact list
+    And I go to import google contacts page
+    And I follow "Invite"
+    Then I should see "ilya@not-werkd.net invited"
+    And 1 email should be delivered to ilya@not-werkd.net
+    And the email should contain "Accept invitation"
 
   Scenario: I want to connect with existing users
-    Given PENDING
+    When I login as "ilya@werkd.net"
+    And google returns my contact list
+    And I go to import google contacts page
+    And I follow "Connect"
+    Then I should see "ilya@not-werkd.net invited"
+    And 1 email should be delivered to ilya@not-werkd.net
+    And the email should contain "Accept invitation"
---
+++ features/step_definitions/import_contacts_stes.rb
@@ -0,0 +1,32 @@
+When(/^google returns my contact list$/) do
+  Rails.cache.stub(:read).and_return(
+    [
+      {
+        :id =>
+          "http://www.google.com/m8/feeds/contacts/Ilya%40challengepost.com/base/7955af58b1a5e0a",
+        :first_name => "Ilya",
+        :last_name => "Werkd",
+        :name => "Ilya Werkd",
+        :email => "ilya@werkd.net",
+        :gender => nil,
+        :birthday => nil,
+        :profile_picture => nil,
+        :relation => nil
+      },
+
+      {
+        :id =>
+          "http://www.google.com/m8/feeds/contacts/Ilya%40challengepost.com/base/7955af58b1a5e0a",
+        :first_name => "Ilya",
+        :last_name => "Notwerkd",
+        :name => "Ilya Notwerked",
+        :email => "ilya@not-werkd.net",
+        :gender => nil,
+        :birthday => nil,
+        :profile_picture => nil,
+        :relation => nil
+      }
+    ]
+
+  )
+end
