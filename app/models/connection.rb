ee932533cd3d72f7470f90d41110af437093d038
Ilya Katz <ilyakatz@gmail.com>
2013-07-10 00:11:29 -0400

    start story to add a new existing connection

--- app/models/connection.rb
+++ app/models/connection.rb
@@ -1,5 +1,6 @@
 class Connection < ActiveRecord::Base
   attr_accessible :connected_to, :user_id
 
-  belongs_to :contacts, class_name: "User", :foreign_key => :connected_to
+  belongs_to :contact, class_name: "User", :foreign_key => :connected_to
+  has_one :user
 end
