50df5ae2bb8086d525872aa5c3de30b1c18fe10a
Ilya Katz <ilyakatz@gmail.com>
2013-07-23 14:25:37 -0400

    can i make deploys faster

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,4 +65,7 @@ WeRKD::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  #http://blog.alexmaccaw.com/faster-deploys?utm_source=rubyweekly&utm_medium=email
+  config.assets.cache_store = :iron_cache
 end
