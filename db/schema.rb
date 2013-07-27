01e8e711118bcf2bf4e4413ee549900a8a33eae7
Ilya Katz <ilyakatz@gmail.com>
2013-07-27 12:36:40 -0400

    add media url to project

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130721014444) do
+ActiveRecord::Schema.define(:version => 20130724123305) do
 
   create_table "authentications", :force => true do |t|
     t.string   "provider"
@@ -60,6 +60,7 @@ ActiveRecord::Schema.define(:version => 20130721014444) do
     t.string   "video"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "media_url"
   end
 
   create_table "users", :force => true do |t|
