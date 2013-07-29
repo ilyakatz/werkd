018ec78a3bc88efd1b67124c1d7a5b1033544b7b
Ilya Katz <ilyakatz@gmail.com>
2013-07-29 09:19:18 -0400

    extract embed video from projects media url

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130724123305) do
+ActiveRecord::Schema.define(:version => 20130729125647) do
 
   create_table "authentications", :force => true do |t|
     t.string   "provider"
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(:version => 20130724123305) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
     t.string   "media_url"
+    t.string   "embed_url"
   end
 
   create_table "users", :force => true do |t|
