f51cb591fec1cd242cb53cb3bc927541c44388b9
Ilya Katz <ilyakatz@gmail.com>
2013-08-19 12:14:00 -0400

    added tests for project preview

---
+++ spec/views/projects/_preview.html.erb_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+describe "projects/_preview.html.erb" do
+
+  let(:project){create(:project)}
+
+  before do
+    view.stub(:project).and_return(project)
+  end
+
+  it "should show video preview" do
+    project.stub(:embed_html).and_return("www.youtube.com/watch?v=ussCHoQttyQ")
+    project.stub(:thumbnail_url).and_return("https://www.google.com/images/srpr/logo4w.pn")
+    render
+    rendered.should match("https://www.google.com/images/srpr/logo4w.pn")
+  end
+
+  it "should show image preview" do
+    project.stub(:thumbnail_url).and_return("https://www.google.com/images/srpr/logo4w.pn")
+    render
+    rendered.should match("https://www.google.com/images/srpr/logo4w.pn")
+  end
+
+  it "should show default image" do
+    render
+    rendered.should match("werkd.png")
+  end
+end
