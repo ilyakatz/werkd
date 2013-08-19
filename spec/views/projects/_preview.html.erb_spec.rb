ebd55f765f38d078ecb7de23be51a993b884a1d6
Ilya Katz <ilyakatz@gmail.com>
2013-08-19 12:56:05 -0400

    check that video can be rendered

--- spec/views/projects/_preview.html.erb_spec.rb
+++ spec/views/projects/_preview.html.erb_spec.rb
@@ -8,11 +8,23 @@ describe "projects/_preview.html.erb" do
     view.stub(:project).and_return(project)
   end
 
-  it "should show video preview" do
-    project.stub(:embed_html).and_return("www.youtube.com/watch?v=ussCHoQttyQ")
-    project.stub(:thumbnail_url).and_return("https://www.google.com/images/srpr/logo4w.pn")
-    render
-    rendered.should match("https://www.google.com/images/srpr/logo4w.pn")
+  context "view present" do
+
+    before do
+      project.stub(:embed_html).and_return("www.youtube.com/watch?v=ussCHoQttyQ")
+      project.stub(:thumbnail_url).and_return("https://www.google.com/images/srpr/logo4w.pn")
+    end
+
+    it "should show thumbnail preview even if video is present" do
+      render
+      rendered.should match("https://www.google.com/images/srpr/logo4w.pn")
+    end
+
+    it "should show video preview" do
+      render "projects/preview", show_video: true
+      rendered.should match("www.youtube.com")
+    end
+
   end
 
   it "should show image preview" do
@@ -25,4 +37,5 @@ describe "projects/_preview.html.erb" do
     render
     rendered.should match("werkd.png")
   end
+
 end
