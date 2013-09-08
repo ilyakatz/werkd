72dbdd562c0677c59565c15d5b909d4d36cd221f
Ilya Scharrenbroich <$EMAIL>
2013-09-08 12:07:22 -0400

    fixing code review comments

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,17 +6,4 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-# include(FactoryGirl::Syntax::Methods)
-
-# # Users:
-
-# user1 = create(:user)
-# user2 = create(:user)
-# user3 = create(:user)
-
-
-# # Projects:
-
-# project1 = create(:project)
-# project2 = create(:project)
-# project3 = create(
+App.create
