8680e7386bdf397506e97b0db200934db1f88acd
Ilya Katz <ilyakatz@gmail.com>
2013-09-24 23:01:29 -0400

    fix homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,12 @@
 class HomeController <DeviseController
 
-  prepend_before_filter :require_no_authentication, only: :index
+  prepend_before_filter :require_no_authentication, :only => [ :index ]
 
   def index
     if user_signed_in?
       redirect_to users_dashboards_path and return
     end
-#    resource = build_resource({})
+    self.resource = User.new
     render layout: false
   end
 
