e72c8483fbd3cc29cdaf55edcf5e809dd50cfe09
Ilya Katz <ilyakatz@gmail.com>
2013-10-06 20:18:00 -0400

    do not show annoying message every time i go to the homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,10 @@
 class HomeController <DeviseController
 
-  prepend_before_filter :require_no_authentication, :only => [ :index ]
+  #prepend_before_filter :require_no_authentication, :only => [ :index ]
 
   def index
     if user_signed_in?
-      redirect_to users_dashboards_path and return
+      redirect_to users_dashboards_url and return
     end
     self.resource = User.new
     render layout: false
